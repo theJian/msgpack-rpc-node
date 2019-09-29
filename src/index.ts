742f6e6016b677488e35e6cdd5ce29168475a0aa
theJian <thejianmail@gmail.com>
2019-09-29 14:06:47 +0800

    Export Tcp transport

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { Server } from './server';
 export { Client } from './client';
 
 export { UnixClient, UnixServer } from './transport/unix';
+export { TcpClient, TcpServer } from './transport/tcp';
 
 export * from './error';
